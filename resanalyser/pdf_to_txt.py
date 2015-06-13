2943a2492a33c080a9bbe7b530d15554ed9366a0
Ashish Patil <ashishnitinpatil@gmail.com>
2015-06-13 11:19:53 +0530

    +encoded pdf to plaintext txt decoder
    Needs pdfminer (20110515), install with
    pip install pdfminer==20110515

---
+++ resanalyser/pdf_to_txt.py
@@ -0,0 +1,25 @@
+from pdfminer.pdfinterp import PDFResourceManager, process_pdf
+from pdfminer.converter import TextConverter
+from pdfminer.layout import LAParams
+from cStringIO import StringIO
+
+def convert_pdf(path):
+
+    rsrcmgr = PDFResourceManager()
+    retstr = StringIO()
+    codec = 'utf-8'
+    laparams = LAParams()
+    device = TextConverter(rsrcmgr, retstr, codec=codec, laparams=laparams)
+
+    fp = file(path, 'rb')
+    process_pdf(rsrcmgr, device, fp)
+    fp.close()
+    device.close()
+
+    str = retstr.getvalue()
+    retstr.close()
+    return str
+
+
+with open('C:\\Users\\ashis\\Desktop\\CIVIL ENGINEERING.txt', 'w') as to_write:
+    to_write.write(convert_pdf('C:\\Users\\ashis\\Desktop\\CIVIL ENGINEERING.pdf'))
